713c5b2230850f70d129fe7cb4fa3c7bc6446bd4
jethass <jethass@hotmail.com>
2017-10-31 23:15:58 +0100

    Update MonException.php

--- Exception/MonException.php
+++ Exception/MonException.php
@@ -19,19 +19,28 @@ function additionner($a, $b)
     throw new MonException('Les deux paramètres doivent être des nombres'); // On lance une exception "MonException".
   }
   
+  if (func_num_args() > 2)
+  {
+    throw new Exception('Pas plus de deux arguments ne doivent être passés à la fonction'); // Cette fois-ci, on lance une exception "Exception".
+  }
+  
   return $a + $b;
 }
 
 try // Nous allons essayer d'effectuer les instructions situées dans ce bloc.
 {
   echo additionner(12, 3), '<br />';
-  echo additionner('azerty', 54), '<br />';
-  echo additionner(4, 8);
+  echo additionner(15, 54, 45), '<br />';
 }
 
 catch (MonException $e) // Nous allons attraper les exceptions "MonException" s'il y en a une qui est levée.
 {
-  echo $e; // On affiche le message d'erreur grâce à la méthode __toString que l'on a écrite.
+  echo '[MonException] : ', $e; // On affiche le message d'erreur grâce à la méthode __toString que l'on a écrite.
 }
 
-echo '<br />Fin du script'; // Ce message s'affiche, ça prouve bien que le script est exécuté jusqu'au bout.
+catch (Exception $e) // Si l'exception n'est toujours pas attrapée, alors nous allons essayer d'attraper l'exception "Exception".
+{
+  echo '[Exception] : ', $e->getMessage(); // La méthode __toString() nous affiche trop d'informations, nous voulons juste le message d'erreur.
+}
+
+echo '<br />Fin du script'; // Ce message s'affiche, cela prouve bien que le script est exécuté jusqu'au bout.
