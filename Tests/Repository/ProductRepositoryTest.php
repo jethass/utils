a10ace9669d89c7758bb9f97b187c35cf4d4599c
jethass <jethass@hotmail.com>
2017-10-31 15:53:10 +0100

    update

---
+++ Tests/Repository/ProductRepositoryTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace AppBundle\Tests\Repository;
+
+use AppBundle\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ProductRepositoryTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->em = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    public function testSearchByCategoryName()
+    {
+        $products = $this->em->getRepository(Product::class) ->searchByCategoryName('foo');
+        $this->assertCount(1, $products);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+}
