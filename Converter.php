3318e4a6bc268e97bd62537f4292fdac14fb3868
jethass <jethass@hotmail.com>
2017-11-27 10:04:42 +0100

    Create Converter.php

---
+++ Converter.php
@@ -0,0 +1,38 @@
+abstract class AbstractConverter
+{
+
+    protected $matches;
+
+    /**
+     * @param array $input
+     * @return array
+     */
+    public function convert(array $input)
+    {
+        $data = array();
+
+        foreach ($input as $key => $value) {
+
+            if (false !== ($keyMatches = array_search($key, $this->matches))) {
+                $data[$keyMatches] = $value;
+            }
+        }
+
+        return $data;
+    }
+}
+
+class CustomerConverter extends AbstractConverter
+{
+
+    protected $matches = array(
+        'lastname' => 'nom',
+        'zipcode' => 'codepostal',
+        'phoneNumber' => 'telephone',
+        'firstname' => 'prenom',
+        'civility' => 'civilite',
+        'email' => 'email',
+        'cmpid' => 'cmpid'
+    );
+
+}
